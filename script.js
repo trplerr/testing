066d5a3d3626c89dda3a68789d851f32af27ec9f
rafaelrawung <162417617+trplerr@users.noreply.github.com>
2024-06-05 16:26:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function JagoankodeStedy() {
+    var jagoankodeblog = document.getElementById("jagoankodeblog");
+    var selengkapnyaText = document.getElementById("selengkapnya");
+    var jagoankodecool = document.getElementById("myjagoankode");
+    
+    if (jagoankodeblog.style.display === "none") {
+      jagoankodeblog.style.display = "inline";
+      jagoankodecool.innerHTML = "Baca selengkapnya"; 
+      selengkapnyaText.style.display = "none";
+    } else {
+      jagoankodeblog.style.display = "none";
+      jagoankodecool.innerHTML = "Sembunyikan"; 
+      selengkapnyaText.style.display = "inline";
+    }
+}
+  
+  function Blogdua() {
+    var jagoankodeblog2 = document.getElementById("jagoankodeblog2");
+    var selengkapnyaText2 = document.getElementById("selengkapnya2");
+    var jagoankodecool2 = document.getElementById("idblogdua");
+  
+    if (jagoankodeblog2.style.display === "none") {
+      jagoankodeblog2.style.display = "inline";
+      jagoankodecool2.innerHTML = "Baca selengkapnya"; 
+      selengkapnyaText2.style.display = "none";
+    } else {
+      jagoankodeblog2.style.display = "none";
+      jagoankodecool2.innerHTML = "Sembunyikan"; 
+      selengkapnyaText2.style.display = "inline";
+    }
+  }
+
+  function Blogtiga() {
+    var jagoankodeblog3 = document.getElementById("jagoankodeblog3");
+    var selengkapnyaText3 = document.getElementById("selengkapnya3");
+    var jagoankodecool3 = document.getElementById("idblogtiga");
+    
+    if (jagoankodeblog3.style.display === "none") {
+      jagoankodeblog3.style.display = "inline";
+      jagoankodecool3.innerHTML = "Baca selengkapnya"; 
+      selengkapnyaText3.style.display = "none";
+    } else {
+      jagoankodeblog3.style.display = "none";
+      jagoankodecool3.innerHTML = "Sembunyikan"; 
+      selengkapnyaText3.style.display = "inline";
+    }
+}
